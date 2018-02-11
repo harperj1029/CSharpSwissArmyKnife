ae4bd9c0a0189e02d424eef91ac2b7a237bf76f3
Jason Harper <harperj1029@gmail.com>
2018-02-11 15:23:58 -0500

    single extension

---
+++ Extensions/ExpandoObjectExtensions.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Dynamic;
+
+namespace CSharpUtilities.Extensions
+{
+    public static class ExpandoObjectExtensions
+    {
+        /// <summary>
+        /// Adds public properties from the source object onto the destination.  If the property already exists
+        /// it will be updated *if* there is a non-null value (thus last actual value wins).
+        /// </summary>
+        /// <param name="destination"></param>
+        /// <param name="source"></param>
+        /// <returns></returns>
+        public static ExpandoObject AddProperties(this ExpandoObject destination, object source)
+        {
+            var sourceType = source.GetType();
+
+            var props = sourceType.GetProperties();
+            var destinationAsDictionary = destination as IDictionary<string, Object>;
+            foreach (var prop in props)
+            {
+                if (destinationAsDictionary.ContainsKey(prop.Name) == false)
+                {
+                    destinationAsDictionary.Add(prop.Name, prop.GetValue(source, null));
+                }
+                else
+                {
+                    var propertyValue = prop.GetValue(source, null);
+                    if (propertyValue != null)
+                    {
+                        destinationAsDictionary[prop.Name] = propertyValue;
+                    }
+                }
+            }
+            return destination;
+        }
+    }
+}
---
+++ UnitTest/ExpandoObjectExtensionsTests.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Dynamic;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Should;
+using CSharpUtilities.Extensions;
+
+namespace UnitTest
+{
+    [TestClass]
+    public class ExpandoObjectExtensionsTests
+    {
+        [TestMethod]
+        public void Should_AddPublicProperties_LastOneWithValueWins()
+        {
+            // Arrange
+            var now = DateTime.UtcNow;
+            var a = new ObjectA { PropertyOne = 1, PropertyTwo = "two" };
+            var b = new ObjectB { PropertyOne = 2, PropertyTwo = null, PropertyThree = now };
+
+            // Act
+            dynamic c = new ExpandoObject().AddProperties(a).AddProperties(b);
+
+            // Assert
+            ((int)c.PropertyOne).ShouldEqual(2);
+            ((string)c.PropertyTwo).ShouldEqual("two");
+            ((DateTime?)c.PropertyThree).ShouldEqual(now);
+            var cAsDictionary = c as IDictionary<string, object>;
+            cAsDictionary.Count.ShouldEqual(3);
+        }     
+
+        private class ObjectA
+        {
+            public ObjectA()
+            {
+                PrivateProperty = "Private property";
+            }
+
+            public int PropertyOne { get; set; }
+            public string PropertyTwo { get; set; }
+            private string PrivateProperty { get; set; }
+        }
+
+        private class ObjectB
+        {
+            public int PropertyOne { get; set; }
+            public string PropertyTwo { get; set; }
+            public DateTime? PropertyThree { get; set; }
+        }
+
+    }
+}
